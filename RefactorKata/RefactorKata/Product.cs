8941be43e00eb32bae622f26e047a44ca3205310
Jessica Reid-Harris <jrharris620@yahoo.com>
2017-10-10 13:59:48 -0500

    Commented on the TODO about Dapper and removed redundancy from the Product file

--- RefactorKata/RefactorKata/Product.cs
+++ RefactorKata/RefactorKata/Product.cs
@@ -2,6 +2,6 @@
 {
     public class Product
     {
-        public string Name { get { return name; } set { name = value; } }
+        public string Name { get; set; }
     }
 }
--- RefactorKata/RefactorKata/Program.cs
+++ RefactorKata/RefactorKata/Program.cs
@@ -18,22 +18,23 @@ namespace RefactorKata
              */
             var reader = cmd.ExecuteReader();
             var products = new List<Product>();
-
+            
+            //Have not yet learned about Dapper @jrharris620
             //TODO: Replace with Dapper
             while (reader.Read())
             {
                 var prod = new Product {Name = reader["Name"].ToString()};
                 products.Add(prod);
             }
-
-            Console.WriteLine("Products Loaded!");
-
-            conn.Dispose();
             
             foreach (var product in products)
             {
                 Console.WriteLine(product.Name);
             }
+
+            Console.WriteLine("Products Loaded!");
+
+            conn.Dispose();
         }
     }
 }
