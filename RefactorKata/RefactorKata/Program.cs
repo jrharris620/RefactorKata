a5401cf10f205c7eaad4c54e88cd916d76938c74
Jessica Reid-Harris <jrharris620@yahoo.com>
2017-10-10 13:40:36 -0500

    Changed explicit types to implicit types

--- RefactorKata/RefactorKata/Program.cs
+++ RefactorKata/RefactorKata/Program.cs
@@ -11,12 +11,12 @@ namespace RefactorKata
             //This is intentionally bad : (  Let's Refactor!
             var conn = new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
 
-            SqlCommand cmd = conn.CreateCommand();
+            var cmd = conn.CreateCommand();
             cmd.CommandText = "select * from Products";
             /*
              * cmd.CommandText = "Select * from Invoices";
              */
-            SqlDataReader reader = cmd.ExecuteReader();
+            var reader = cmd.ExecuteReader();
             List<Product> products = new List<Product>();
 
             //TODO: Replace with Dapper
